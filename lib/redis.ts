766d2749f6008ec89b98f6cda1009975ccde4ae0
CodingRI <125729609+CodingRI@users.noreply.github.com>
2026-04-04 19:36:52 +0530

    using upstash once again

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,15 +2,27 @@ import IORedis from "ioredis";
 
 const isProd = process.env.NODE_ENV === "production";
 
-export const connection = isProd
-  ? new IORedis({
-      host: process.env.UPSTASH_REDIS_HOST,
-      port: Number(process.env.UPSTASH_REDIS_PORT),
-      password: process.env.UPSTASH_REDIS_PASSWORD,
-      tls: {},
+// export const connection = isProd
+//   ? new IORedis({
+//       host: process.env.UPSTASH_REDIS_HOST,
+//       port: Number(process.env.UPSTASH_REDIS_PORT),
+//       password: process.env.UPSTASH_REDIS_PASSWORD,
+//       tls: {},
 
-      maxRetriesPerRequest: null,
-    })
-  : new IORedis("redis://127.0.0.1:6379", {
-      maxRetriesPerRequest: null,
-    });
+//       maxRetriesPerRequest: null,
+//     })
+//   : new IORedis("redis://127.0.0.1:6379", {
+//       maxRetriesPerRequest: null,
+//     });
+
+
+    export const connection = 
+        new IORedis({
+        host: process.env.UPSTASH_REDIS_HOST,
+        port: Number(process.env.UPSTASH_REDIS_PORT),
+        password: process.env.UPSTASH_REDIS_PASSWORD,
+        tls: {},
+  
+        maxRetriesPerRequest: null,
+      })
+   
