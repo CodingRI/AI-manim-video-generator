ac07fba8d1ccd1963cba28e2c9f5cf41c1d2672e
CodingRI <125729609+CodingRI@users.noreply.github.com>
2026-04-21 19:06:23 +0530

    typo error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { redirect } from "next/navigation";
 export default function Home() {
-  redirect("/welcomepage");
+  redirect("/welcomePage");
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       
